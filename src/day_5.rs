cfe752b456011545a86c481518d42321ed3b71b4
Arion Roberto Krause <arionkrause@trt9.jus.br>
2019-03-25 12:58:14 -0300

    Day 5 Part 1

---
+++ src/day_5.rs
@@ -0,0 +1,43 @@
+use super::print_day;
+
+pub fn solve(input: &str) {
+    print_day!(file!());
+    println!("Part 1: {}.", part_1::solve(&input));
+    println!();
+}
+
+mod part_1 {
+    pub fn solve(input: &str) -> usize {
+        let mut jumps: Vec<isize> = input
+            .lines()
+            .map(|line| line.parse::<isize>().unwrap())
+            .collect();
+
+        let mut pc = 0;
+        let mut steps = 0;
+
+        loop {
+            let new_pc = pc + jumps[pc as usize];
+            steps += 1;
+
+            if new_pc >= jumps.len() as isize {
+                return steps;
+            }
+
+            jumps[pc as usize] += 1;
+            pc = new_pc;
+        }
+    }
+
+    #[cfg(test)]
+    #[test]
+    fn test_1() {
+        let input = "0
+3
+0
+1
+-3";
+
+        assert_eq!(solve(input), 5);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ mod day_1;
 mod day_2;
 mod day_3;
 mod day_4;
-//mod day_5;
+mod day_5;
 //mod day_6;
 //mod day_7;
 //mod day_8;
@@ -63,10 +63,10 @@ fn main() -> Result<(), Error> {
         day_4::solve(&read_file("day_4")?);
     }
 
-    //    if run_all || &arguments[1] == "day_5" {
-    //        day_5::solve(&read_file("day_5")?);
-    //    }
-    //
+    if run_all || &arguments[1] == "day_5" {
+        day_5::solve(&read_file("day_5")?);
+    }
+
     //    if run_all || &arguments[1] == "day_6" {
     //        day_6::solve(&read_file("day_6")?);
     //    }
