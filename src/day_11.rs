d7d7b4ea6b283a199dc47bd6be64f2fd0c7ac023
Arion Roberto Krause <arionkrause@trt9.jus.br>
2019-04-05 09:42:38 -0300

    Day 11 Part 2

--- src/day_11.rs
+++ src/day_11.rs
@@ -3,6 +3,7 @@ use super::print_day;
 pub fn solve(input: &str) {
     print_day!(file!());
     println!("Part 1: {}.", part_1::solve(&input));
+    println!("Part 2: {}.", part_2::solve(&input));
     println!();
 }
 
@@ -95,5 +96,52 @@ mod part_1 {
     #[test]
     fn test_4() {
         assert_eq!(solve("se,sw,se,sw,sw"), 3);
+    }
+}
+
+mod part_2 {
+    use crate::day_11::{decode_input, Direction, Position};
+
+    pub fn solve(input: &str) -> usize {
+        let mut position = Position { x: 0, y: 0, z: 0 };
+        let mut maximum_amount_steps = 0;
+
+        decode_input(&input).iter().for_each(|step| {
+            match step {
+                Direction::North => {
+                    position.y += 1;
+                    position.z -= 1;
+                }
+                Direction::Northeast => {
+                    position.x += 1;
+                    position.z -= 1;
+                }
+                Direction::Southeast => {
+                    position.x += 1;
+                    position.y -= 1;
+                }
+                Direction::South => {
+                    position.y -= 1;
+                    position.z += 1;
+                }
+                Direction::Southwest => {
+                    position.x -= 1;
+                    position.z += 1;
+                }
+                Direction::Northwest => {
+                    position.x -= 1;
+                    position.y += 1;
+                }
+            };
+
+            let amount_steps =
+                ((position.x.abs() + position.y.abs() + position.z.abs()) / 2) as usize;
+
+            if amount_steps > maximum_amount_steps {
+                maximum_amount_steps = amount_steps;
+            }
+        });
+
+        maximum_amount_steps
     }
 }
