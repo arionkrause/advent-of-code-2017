39b4c2724bb3b74c32171967c80e0037d1de2a94
Arion Roberto Krause <arionkrause@trt9.jus.br>
2019-05-20 09:49:04 -0300

    Day 15 Part 2

--- src/day_15.rs
+++ src/day_15.rs
@@ -4,6 +4,7 @@ use regex::Regex;
 pub fn solve(input: &str) {
     print_day!(file!());
     println!("Part 1: {}.", part_1::solve(&input));
+    println!("Part 2: {}.", part_2::solve(&input));
     println!();
 }
 
@@ -52,5 +53,46 @@ mod part_1 {
 Generator B starts with 8921";
 
         assert_eq!(solve(&input), 588);
+    }
+}
+
+mod part_2 {
+    use crate::day_15::decode_input;
+
+    pub fn solve(input: &str) -> usize {
+        let (mut value_a, mut value_b) = decode_input(&input);
+        let mut amount_matches = 0;
+
+        for _ in 0..5_000_000 {
+            value_a = get_next_value(value_a, 16807, 4);
+            value_b = get_next_value(value_b, 48271, 8);
+
+            if value_a & 0b1111_1111_1111_1111 == value_b & 0b1111_1111_1111_1111 {
+                amount_matches += 1;
+            }
+        }
+
+        amount_matches
+    }
+
+    fn get_next_value(current_value: usize, factor: usize, multiple_of: usize) -> usize {
+        let mut new_value = current_value;
+
+        loop {
+            new_value = new_value * factor % 2_147_483_647;
+
+            if new_value % multiple_of == 0 {
+                return new_value;
+            }
+        }
+    }
+
+    #[cfg(test)]
+    #[test]
+    fn test_1() {
+        let input = "Generator A starts with 65
+Generator B starts with 8921";
+
+        assert_eq!(solve(&input), 309);
     }
 }
