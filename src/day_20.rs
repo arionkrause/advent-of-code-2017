04d5e27233ece4a3136b0f1ca715e4b4ff528a97
Arion Roberto Krause <arionkrause@trt9.jus.br>
2019-05-22 15:12:04 -0300

    Day 20 Part 2

--- src/day_20.rs
+++ src/day_20.rs
@@ -4,11 +4,13 @@ use regex::Regex;
 pub fn solve(input: &str) {
     print_day!(file!());
     println!("Part 1: {}.", part_1::solve(&input));
+    println!("Part 2: {}.", part_2::solve(&input));
     println!();
 }
 
 #[derive(Debug)]
 struct Particle {
+    id: usize,
     position: Position,
     velocity: Velocity,
     acceleration: Acceleration,
@@ -29,7 +31,7 @@ impl Particle {
     }
 }
 
-#[derive(Debug)]
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
 struct Position {
     x: isize,
     y: isize,
@@ -56,9 +58,11 @@ fn decode_input(input: &str) -> Vec<Particle> {
 
     input
         .lines()
-        .map(|line| {
+        .enumerate()
+        .map(|(index, line)| {
             let captures = re.captures(&line).unwrap();
             Particle {
+                id: index,
                 position: Position {
                     x: captures
                         .name("position_x")
@@ -130,21 +134,38 @@ mod part_1 {
 
     pub fn solve(input: &str) -> usize {
         let mut particles = decode_input(&input);
+        let amount_updates_without_change_to_consider_finished = 256;
+        let mut amount_updates_without_change_so_far = 0;
+        let mut last_closest_particle_id_to_0_0_0 = None;
 
-        for _ in 0..1000 {
+        loop {
             particles.iter_mut().for_each(day_20::Particle::update);
-        }
 
-        particles
-            .iter()
-            .enumerate()
-            .min_by(|(_, particle_a), (_, particle_b)| {
-                particle_a
-                    .get_manhattan_distance()
-                    .cmp(&particle_b.get_manhattan_distance())
-            })
-            .unwrap()
-            .0
+            let current_closest_particle_to_0_0_0 = particles
+                .iter()
+                .min_by(|particle_a, particle_b| {
+                    particle_a
+                        .get_manhattan_distance()
+                        .cmp(&particle_b.get_manhattan_distance())
+                })
+                .unwrap();
+
+            if last_closest_particle_id_to_0_0_0.is_some()
+                && current_closest_particle_to_0_0_0.id
+                    == last_closest_particle_id_to_0_0_0.unwrap()
+            {
+                amount_updates_without_change_so_far += 1;
+
+                if amount_updates_without_change_so_far
+                    == amount_updates_without_change_to_consider_finished
+                {
+                    return current_closest_particle_to_0_0_0.id;
+                }
+            } else {
+                last_closest_particle_id_to_0_0_0 = Some(current_closest_particle_to_0_0_0.id);
+                amount_updates_without_change_so_far = 0;
+            }
+        }
     }
 
     #[cfg(test)]
@@ -154,5 +175,62 @@ mod part_1 {
 p=<4,0,0>, v=<0,0,0>, a=<-2,0,0>";
 
         assert_eq!(solve(input), 0);
+    }
+}
+
+mod part_2 {
+    use crate::day_20;
+    use crate::day_20::decode_input;
+    use std::collections::{HashMap, HashSet};
+
+    pub fn solve(input: &str) -> usize {
+        let mut particles = decode_input(&input);
+        let amount_updates_without_collisions_to_consider_finished = 16;
+        let mut amount_updates_without_collisions_so_far = 0;
+
+        loop {
+            particles.iter_mut().for_each(day_20::Particle::update);
+            let mut indices_collided_particles = HashSet::new();
+            let mut positions = HashMap::new();
+
+            for particle in particles.iter() {
+                match positions.get(&particle.position) {
+                    None => {
+                        positions.insert(particle.position.clone(), particle.id);
+                    }
+                    Some(other_particle_id) => {
+                        indices_collided_particles.insert(particle.id);
+                        indices_collided_particles.insert(*other_particle_id);
+                    }
+                }
+            }
+
+            if indices_collided_particles.is_empty() {
+                amount_updates_without_collisions_so_far += 1;
+
+                if amount_updates_without_collisions_so_far
+                    == amount_updates_without_collisions_to_consider_finished
+                {
+                    break;
+                }
+            } else {
+                amount_updates_without_collisions_so_far = 0;
+            }
+
+            particles.retain(|particle| !indices_collided_particles.contains(&particle.id));
+        }
+
+        particles.len()
+    }
+
+    #[cfg(test)]
+    #[test]
+    fn test_1() {
+        let input = "p=<-6,0,0>, v=<3,0,0>, a=<0,0,0>
+p=<-4,0,0>, v=<2,0,0>, a=<0,0,0>
+p=<-2,0,0>, v=<1,0,0>, a=<0,0,0>
+p=<3,0,0>, v=<-1,0,0>, a=<0,0,0>";
+
+        assert_eq!(solve(input), 1);
     }
 }
